9a6f386a3e496ea9131655511b1e55ff85954d8b
Anisha Jain <ajain64@uw.edu>
2026-04-17 03:47:19 +0000

    fix: Fix config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anishajain734.github.io',
-  base: '/website'
+  base: '/website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
